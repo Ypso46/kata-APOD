bfe39208da9d2c722aff89a70d7e2cd6c033c6eb
Greg <gd.temp@pm.me>
2023-03-07 16:41:57 +0100

    Added NASA's API KEY and some minor refactor

--- script.js
+++ script.js
@@ -1,21 +1,45 @@
+const url = 'https://api.nasa.gov/planetary/apod?api_key=';
+const api_key = config.NASA_API_KEY;
 const fetchNASAData = async () => {
     try {
-        const response = await fetch('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY');
+        let date = "";
+        const response = await fetch(`${url}${api_key}`);
         const data = await response.json();
         console.log(data);
-        displayData(data);
+        displayTitle(data);
+        displayDate(data);
+        displayPicture(data);
+        displayExplanation(data);
     } catch (error) {
         console.log(error);
     }
 }
 
-const displayData = data => {
-    document.getElementById('title').textContent = data.title;
-    document.getElementById('date').textContent = data.date;
-    document.getElementById('picture').src = data.hdurl;
-    document.getElementById('explanation').textContent = data.explanation;
-}
-
 fetchNASAData();
+
+const displayTitle = data => {document.getElementById('title').textContent = data.title; }
+const displayDate = data => {document.getElementById('date').textContent = data.date; }
+const displayPicture = data => {document.getElementById('picture').src = data.hdurl; }
+const displayExplanation = data => {document.getElementById('explanation').innerHTML = '<span id="expl">Explanation:</span> ' + data.explanation; }
+
+/*
+const date = Date();
+/*
+function getDate(date) {
+    const dateBefore = toISOString(date - 1);
+    document.getElementById('back').addEventListener('click', () => {
+        return dateBefore;
+    });
+    if (dateAfter === date) {
+        console.log("Error");
+    } else {
+        const dateAfter = toISOString(date + 1);
+        document.getElementById('next').addEventListener('click', () => {
+            return dateAfter;
+        });
+    } 
+}
+*/
+
 
 
